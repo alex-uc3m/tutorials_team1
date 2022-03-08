ed024bc14543f263433d7e0e2004e5507b033615
Nada Helali <nada.tunisia@gmail.com>
2022-03-08 19:23:05 +0100

    Added 4 initial functions

---
+++ main.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+if __name__ == '__main__':
+    x, y = 12, 4
+    print(add(x, y))
+    print(subtract(x, y))
+    print(multiply(x, y))
+    print(divide(x, y))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
