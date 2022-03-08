c6e05a841cff4b8c687e0d3db76ef326d96ae9e3
alex-uc3m <98971909+alex-uc3m@users.noreply.github.com>
2022-03-08 19:36:30 +0100

    Add test case for subtract

--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
 import unittest
 from unittest import TestCase
-from main import add
+from main import add, subtract
 
 class Test(TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+        
+    def test_subtract(self):
+        self.assertEqual(subtract(4, 1), 3)
 
 if __name__=="__main__":
     unittest.main()
